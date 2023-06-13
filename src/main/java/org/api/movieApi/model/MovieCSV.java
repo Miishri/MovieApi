d3fa56dbd99173871ec7452a94624fa9a690edef
Muhammad.PC <muhammadshamaeem@gmail.com>
2023-06-13 16:21:19 +0200

    add movie csv model

---
+++ src/main/java/org/api/movieApi/model/MovieCSV.java
@@ -0,0 +1,85 @@
+package org.api.movieApi.model;
+
+import com.opencsv.bean.CsvBindByName;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MovieCSV {
+
+    @CsvBindByName
+    private boolean adult;
+
+    @CsvBindByName(column = "belongs_to_collection")
+    private String belongsToCollection;
+
+    @CsvBindByName
+    private int budget;
+
+    @CsvBindByName
+    private String genres;
+
+    @CsvBindByName
+    private String homepage;
+
+    @CsvBindByName
+    private int id;
+
+    @CsvBindByName(column = "imdb_id")
+    private String imdbId;
+
+    @CsvBindByName(column = "original_language")
+    private String originalLanguage;
+
+    @CsvBindByName(column = "original_title")
+    private String originalTitle;
+
+    @CsvBindByName
+    private String overview;
+
+    @CsvBindByName
+    private double popularity;
+
+    @CsvBindByName(column = "poster_path")
+    private String posterPath;
+
+    @CsvBindByName(column = "production_companies")
+    private String productionCompanies;
+
+    @CsvBindByName(column = "production_countries")
+    private String productionCountries;
+
+    @CsvBindByName(column = "release_date")
+    private String releaseDate;
+
+    @CsvBindByName
+    private long revenue;
+
+    @CsvBindByName
+    private double runtime;
+
+    @CsvBindByName(column = "spoken_languages")
+    private String spokenLanguages;
+
+    @CsvBindByName
+    private String status;
+
+    @CsvBindByName
+    private String tagline;
+
+    @CsvBindByName
+    private String title;
+
+    @CsvBindByName
+    private boolean video;
+
+    @CsvBindByName(column = "vote_average")
+    private double voteAverage;
+
+    @CsvBindByName(column = "vote_count")
+    private int voteCount;
+}
