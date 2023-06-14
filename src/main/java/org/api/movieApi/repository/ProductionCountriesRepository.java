984715c6d1e8bc3e2bb5602fee12681a20af252a
Muhammad.PC <muhammadshamaeem@gmail.com>
2023-06-14 02:50:41 +0200

    rename to production

--- src/main/java/org/api/movieApi/repository/ProductionCountriesRepository.java
+++ src/main/java/org/api/movieApi/repository/ProductionCountriesRepository.java
@@ -3,5 +3,5 @@ package org.api.movieApi.repository;
 import org.api.movieApi.entities.ProductionCountries;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductCountriesRepository extends JpaRepository<ProductionCountries, Long> {
+public interface ProductionCountriesRepository extends JpaRepository<ProductionCountries, Long> {
 }
