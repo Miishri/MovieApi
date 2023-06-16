a23ad1deea19cb97306858a6d2336ad871aa1339
Muhammad.PC <muhammadshamaeem@gmail.com>
2023-06-16 02:36:03 +0200

    fix class path import

--- src/main/java/org/api/movieApi/repository/MovieRepository.java
+++ src/main/java/org/api/movieApi/repository/MovieRepository.java
@@ -1,7 +1,18 @@
 package org.api.movieApi.repository;
 
 import org.api.movieApi.entities.Movie;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MovieRepository extends JpaRepository<Movie, Long> {
+
+    Page<Movie> findAllByTitle(String title,
+                               Pageable pageable);
+
+    Page<Movie> findAllByRuntime(Double runtime, Pageable pageable);
+
+    Page<Movie> findAllByOriginalLanguage(String originalLanguage, Pageable pageable);
+
+    Page<Movie> findAllByAdult(Boolean adult, Pageable pageable);
 }
