eb136d2b4ac7c0acc747daff702a4135127e56a3
Muhammad.PC <muhammadshamaeem@gmail.com>
2023-06-16 02:35:47 +0200

    fix inline

--- src/main/java/org/api/movieApi/services/MovieCsvServiceImpl.java
+++ src/main/java/org/api/movieApi/services/MovieCsvServiceImpl.java
@@ -2,7 +2,6 @@ package org.api.movieApi.services;
 
 import com.opencsv.bean.CsvToBeanBuilder;
 import org.api.movieApi.model.MovieCSV;
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
@@ -11,7 +10,6 @@ import java.io.FileReader;
 import java.util.List;
 
 @Service
-@Primary
 public class MovieCsvServiceImpl implements MovieCsvService {
     @Override
     public List<MovieCSV> convertToMovieCsv(File csvMovieFile) {
